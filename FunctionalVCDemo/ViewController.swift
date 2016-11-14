5f2b7f16af500d5ecd7127f0ce80268fa318ead3
Olivier Halligon <olivier@halligon.net>
2016-11-14 21:46:44 +0100

    Initial Main VC

--- FunctionalVCDemo/ViewController.swift
+++ FunctionalVCDemo/ViewController.swift
@@ -15,11 +15,13 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func askForText(_ sender: UIButton) {
   }
 
-
+  @IBAction func askForNumber(_ sender: UIButton) {
+  }
+  
+  @IBAction func startTutorial(_ sender: UIButton) {
+  }
 }
 
